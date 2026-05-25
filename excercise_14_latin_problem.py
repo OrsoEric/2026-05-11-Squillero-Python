3d813c41c9f479ed918dacdad5281ab99b107f0c
FoM <30684972+OrsoEric@users.noreply.github.com>
2026-05-25 17:03:05 +0200

    this logic detection is not trivial to do

--- excercise_14_latin_problem.py
+++ excercise_14_latin_problem.py
@@ -79,6 +79,11 @@ class St_solution:
             if n_index not in (0, 1, 2):
                 raise ValueError("Each position must be 0, 1, or 2")
 
+    def copy(self) -> "St_solution":
+        return St_solution(
+            ln_location=self.ln_location.copy()
+        )
+
     def get(self, i_e_agent: E_agent) -> int:
         return self.ln_location[i_e_agent]
 
@@ -86,6 +91,9 @@ class St_solution:
         if ln_location not in (0, 1, 2):
             raise ValueError("Value must be 0, 1, or 2")
         self.ln_location[i_e_agent] = i_n_value
+
+    def get_num_location(self, i_e_location : E_location ) -> int:
+        return sum(1 for n_location in self.ln_location if n_location == i_e_location)
 
     def is_invalid(self)->bool:
         """
@@ -105,7 +113,8 @@ class St_solution:
         # -------------------------------------------------
         # Rule 1: boat capacity
         # -------------------------------------------------
-        if sum(1 for n_location in self.ln_location if n_location == E_location.BOAT) > 2:
+
+        if self.get_num_location( E_location.BOAT ) > 2:
             return True #INVALID
 
         # -------------------------------------------------
@@ -113,18 +122,98 @@ class St_solution:
         # -------------------------------------------------
         # for all pairs, if the male and female are in same place, that's valid
 
+        
         if self.ln_location[E_agent.MA] == self.ln_location[E_agent.FA] and self.ln_location[E_agent.MB] == self.ln_location[E_agent.FB] and self.ln_location[E_agent.MC] == self.ln_location[E_agent.FC]:
             return False #VALID
+        
+
+        # -------------------------------------------------
+        # Rule 3B: female without her paired male, and with a unpaired male
+        # -------------------------------------------------
+        
+        """
+        this is overly sensitive, it doesn't count the save from other pairs in the same location
+        [0, 0, 0, 1, 0, 0]
+        [0, 0, 0, 0, 1, 0]
+        [0, 0, 0, 0, 0, 1]
+        """
+
+        """
+        #if the female is not with her paired male
+        if self.ln_location[E_agent.FA] != self.ln_location[E_agent.MA]:
+            #and the female is with an unpaired male
+            if self.ln_location[E_agent.FA] == self.ln_location[E_agent.MB]:
+                return True #INVALID  
+            if self.ln_location[E_agent.FA] == self.ln_location[E_agent.MC]:
+                return True #INVALID  
+
+
+        if self.ln_location[E_agent.FB] != self.ln_location[E_agent.MB]:
+            #and the female is with an unpaired male
+            if self.ln_location[E_agent.FB] == self.ln_location[E_agent.MA]:
+                return True #INVALID  
+            if self.ln_location[E_agent.FB] == self.ln_location[E_agent.MC]:
+                return True #INVALID  
+
+        if self.ln_location[E_agent.FC] != self.ln_location[E_agent.MC]:
+            #and the female is with an unpaired male
+            if self.ln_location[E_agent.FC] == self.ln_location[E_agent.MA]:
+                return True #INVALID  
+            if self.ln_location[E_agent.FC] == self.ln_location[E_agent.MB]:
+                return True #INVALID  
+        """
 
         # -------------------------------------------------
         # Rule 3: all females together
         # -------------------------------------------------
 
+        
         if self.ln_location[E_agent.FA] == self.ln_location[E_agent.FB] and self.ln_location[E_agent.FA] == self.ln_location[E_agent.FC]:
             return False #VALID
+        
+
+        return False #VALID
+
+    def load_boat(self) -> List:
+        """
+        Enumerate all valid boat loading operations
 
 
-        return True #INVALID
+
+        """
+        
+        #number of people on the boat
+        n_num_in_boat = self.get_num_location( E_location.BOAT )
+
+        ln_possible_load_move = list()
+
+        if n_num_in_boat >= 2:
+            #no move possible
+            return ln_possible_load_move
+
+        #for each agent, if that agent is START, make a solution with that agent as boat
+        for e_agent in E_agent:
+            if self.ln_location[e_agent] == E_location.START:
+                ln_copy = self.copy()
+                ln_copy.ln_location[e_agent] = E_location.BOAT
+
+                #print(ln_copy)
+                #if it's valid
+                if ln_copy.is_invalid() == False:
+                    #possible load operation
+                    ln_possible_load_move.append(ln_copy)
+
+        if n_num_in_boat == 0:
+            #for each solution where one was moved
+            #for ln_
+
+
+
+            #no move possible
+            return ln_possible_load_move
+
+
+        return ln_possible_load_move
 
     def __str__(self) -> str:
         # converts: [E_location.START, E_location.BOAT, ...]
@@ -161,8 +250,19 @@ def list_valid_states():
     print(f"VALID: {n_valid}")
     print(f"INVALID: {n_invalid}")
 
+def list_valid_load_operation():
+    n_valid : int = 0
+    n_invalid : int = 0
+
+    st_solution = St_solution()
+    lln_load_operation = st_solution.load_boat()
+
+    for ln_load_operation in lln_load_operation:
+        print(ln_load_operation)
 
 if __name__ == "__main__":
     #test_bench()
 
     list_valid_states()
+
+    #list_valid_load_operation()
