2f8d2d006a47add4992d3ad2fb500c8e2e46d8a0
FoM <30684972+OrsoEric@users.noreply.github.com>
2026-05-25 15:48:47 +0200

    lesson and formulation of tree couple probelm

---
+++ excercise_14_latin_problem.py
@@ -0,0 +1,21 @@
+"""
+“Tres fratres erant qui singulas sorores habebant, et fluvium transire debebant (erat enim unicuique illorum concupiscientia in sorore proximi sui), qui venientes ad fluvium non invenerunt nisi parvam naviculam, in qua non potuerunt amplius nisi duo ex illis transire. Dicat, qui potest, qualiter fluvium transierunt, ne una quidem earum ex ipsis maculata sit?”
+"""
+
+"""
+“There were three brothers who each had one sister, and they had to cross a river (for each of them desired the sister of the one next to him).
+When they came to the river, they found only a small boat, in which no more than two of them could cross at a time.
+Let him say, who can, how they crossed the river so that not even one of the sisters was dishonored?”
+"""
+
+"""
+- tre coppie fratelli/sorelle
+- barca con due posti per attraversare il fiume
+- combinazioni che devono essere valide su lato A, lato B e barca sempre
+- possono essere solo uomini assieme
+- possono essere solo femmine assieme
+- possono essere una coppia fratello/sorella piu' ogni altra combinazione
+- tutte le altre cobinazioni sono proibite
+
+traduci in un grafo
+"""
---
+++ lesson_connectivity.md
@@ -0,0 +1,41 @@
+# Connectivity
+
+create one subclass for each node (number)
+iterate each link
+rename the node to the lowest adjacent
+i rename all the same istances of the same color
+
+
+# Prim
+
+Nodes have coordinates
+
+at each stape get to the cheapest node
+
+this makes a line with branckes that connects to every nodes
+
+Spanning Tree
+
+# Kruscal Algorithm
+
+Links arks (not connected) as long as they don't make loops
+
+Keep going until everything is connected
+
+Minimum SPanning Tree
+
+# Coloring Graph
+
+assign colors to nodes so that two colors are not adjacent
+
+# Traveling Salesman Problen
+
+# graphlib
+
+it' a standard library, it's meant to do dependencies of task in linux
+
+# NetworkX
+
+python library for networks and standard graph algorithms
+
+based on dictionaries
